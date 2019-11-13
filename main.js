7caa49e7e8faa89fdfc8ed0a018dc7b8a9c8b5bf
Angel Burgos <angelpublicista.1@gmail.com>
2019-11-13 11:06:47 -0500

    Finalización

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 var cont_dptos = document.querySelector('#dptos');
+var cont_mun = document.querySelector('#municipios');
 
-fetch('http://localhost/api/ciudades_dptos/departamentos.php')
+fetch('https://univercity.com.co/api_colombia/departamentos.php')
+
 .then(res => res.json())
 .then(data => {
     for (let dpto of data) {
-
         cont_dptos.innerHTML += `
             <option value="${dpto.id_departamento}">${dpto.departamento}</option>
         `
     }
 })
 
+cont_dptos.addEventListener('change', function(e) {
+    cont_mun.innerHTML = '<option selected>Selecciona un municipio</option>';
+    
+    let valor_dpto = e.target.value;
 
-function cargarMunicipios(){
-    console.log(this);
-}
+    fetch('https://univercity.com.co/api_colombia/municipios.php?id_departamento=' + valor_dpto)
+
+    .then(res => res.json())
+    .then(data => {
+        for (let mun of data) {
+            cont_mun.innerHTML += `
+                <option value="${mun.id_municipio}">${mun.municipio}</option>
+            `
+        }
+    })
+})
+
+
+
+
+
+
+
